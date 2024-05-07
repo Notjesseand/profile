f4d288a1fe0a525ca5d0aaec5d1e78964f95e858
Notjessand <jessennorom@gmail.com>
2024-05-07 10:51:43 -0500

    additional updates

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function nav() {
   return (
-    <div className="fixed w-full z-50 flex sm:px-20 py-7 md:py-5 justify-between px-5 bg-white">
+    <div className="fixed w-full z-50 flex sm:px-20 py-6 sm:py-7 md:py-5 justify-between px-5 bg-white">
       <Link href={"/"} className="">
         <img src="/logo.png" className="sm:h-11 h-8 flex items-center mt-1" />
       </Link>
