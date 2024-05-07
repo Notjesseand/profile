bb6bd95839861c0ff2429bf36fdec82d00b222b2
Notjessand <jessennorom@gmail.com>
2024-05-07 08:55:27 -0500

    additional updates

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,12 +1,15 @@
-"use client"
-import React from 'react'
-import { Sidebar } from './sidebar'
+"use client";
+import React from "react";
+import { Sidebar } from "./sidebar";
+import Link from "next/link";
 
 export default function nav() {
   return (
-    <div className='fixed w-full z-50 flex sm:px-20 py-7 md:py-5 justify-between px-5 bg-white'>
-      <img src='/logo.png' className='sm:h-11 h-8 flex items-center mt-1'/>
-      <Sidebar/>
+    <div className="fixed w-full z-50 flex sm:px-20 py-7 md:py-5 justify-between px-5 bg-white">
+      <Link href={"/"} className="">
+        <img src="/logo.png" className="sm:h-11 h-8 flex items-center mt-1" />
+      </Link>
+      <Sidebar />
     </div>
-  )
+  );
 }
