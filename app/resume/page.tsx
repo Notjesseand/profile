4ee6a375e5c884ada5e97119b711684954eee276
Notjessand <jessennorom@gmail.com>
2024-05-28 11:34:28 -0500

    addition of more animations

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import React from "react";
 import Nav from "@/components/nav";
 import Support from "@/components/support";
 import Contact from "@/components/contact";
 import Link from "next/link";
+import useOnScreen from "@/hooks/scroll";
+import { variantLeftToRight } from "@/hooks/variants";
+import { variantRightToLeft } from "@/hooks/variants";
+import { motion } from "framer-motion";
 
 export default function Page() {
+  const [ref, isVisible] = useOnScreen({ threshold: 0.1 });
+  const [ref2, isVisible2] = useOnScreen({ threshold: 0.1 });
+
   return (
-    <div>
+    <div className="overflow-hidden ">
       <Nav />
       <div className="pt-24"></div>
       <div className="px-6 md:px-20 pt-6 md:pt-12">
@@ -16,88 +24,105 @@ export default function Page() {
         {/* experiences and eduction */}
         <div className="grid md:grid-cols-2 gap-x-20">
           {/* experience */}
-          <div className="pt-8">
-            <div className="font-custom font-semibold text-xl text-slate-600 border-b-2 pb-2 mb-4 w-full flex">
-              Experience
+          <motion.div
+            // @ts-ignore
+            ref={ref}
+            initial="hidden"
+            animate={isVisible ? "visible" : "hidden"}
+            variants={variantLeftToRight}
+            transition={{ duration: 1 }}
+          >
+            <div className="pt-8">
+              <div className="font-custom font-semibold text-xl text-slate-600 border-b-2 pb-2 mb-4 w-full flex">
+                Experience
+              </div>
+              {/* scholarly */}
+              <div className="w-full py-3 flex items-center justify-between">
+                <div>
+                  <p className="text-lg font-semibold">Scolarly</p>
+                  <p className="text-slate-500">Web Developer</p>
+                </div>
+                {/* duration */}
+                <div className="border-2 px-4 py-1 rounded text-slate-500">
+                  2023
+                </div>
+              </div>
+              {/* Abia Tech Hub */}
+              <div className="w-full py-3 flex items-center justify-between">
+                <div>
+                  <p className="text-lg font-semibold">Abia Tech Hub</p>
+                  <p className="text-slate-500">Intern and Web Developer</p>
+                </div>
+                {/* duration */}
+                <div className="border-2 px-4 py-1 rounded text-slate-500">
+                  2023-2023
+                </div>
+              </div>
+              {/* ECR Tech */}
+              <div className="w-full py-3 flex items-center justify-between">
+                <div>
+                  <p className="text-lg font-semibold">ECR Tech</p>
+                  <p className="text-slate-500">Intern</p>
+                </div>
+                {/* duration */}
+                <div className="border-2 px-4 py-1 rounded text-slate-500">
+                  2019-2021
+                </div>
+              </div>
             </div>
-            {/* scholarly */}
-            <div className="w-full py-3 flex items-center justify-between">
-              <div>
-                <p className="text-lg font-semibold">Scolarly</p>
-                <p className="text-slate-500">Web Developer</p>
-              </div>
-              {/* duration */}
-              <div className="border-2 px-4 py-1 rounded text-slate-500">
-                2023
-              </div>
-            </div>
-            {/* Abia Tech Hub */}
-            <div className="w-full py-3 flex items-center justify-between">
-              <div>
-                <p className="text-lg font-semibold">Abia Tech Hub</p>
-                <p className="text-slate-500">Intern and Web Developer</p>
-              </div>
-              {/* duration */}
-              <div className="border-2 px-4 py-1 rounded text-slate-500">
-                2023-2023
-              </div>
-            </div>
-            {/* ECR Tech */}
-            <div className="w-full py-3 flex items-center justify-between">
-              <div>
-                <p className="text-lg font-semibold">ECR Tech</p>
-                <p className="text-slate-500">Intern</p>
-              </div>
-              {/* duration */}
-              <div className="border-2 px-4 py-1 rounded text-slate-500">
-                2019-2021
-              </div>
-            </div>
-          </div>
-
+          </motion.div>
           {/* eductation */}
-          <div className="pt-8">
-            <div className="font-custom font-semibold text-xl text-slate-600 border-b-2 pb-2 mb-4 w-full flex">
-              Education
+          <motion.div
+            // @ts-ignore
+            ref={ref}
+            initial="hidden"
+            animate={isVisible ? "visible" : "hidden"}
+            variants={variantRightToLeft}
+            transition={{ duration: 1 }}
+          >
+            <div className="pt-8">
+              <div className="font-custom font-semibold text-xl text-slate-600 border-b-2 pb-2 mb-4 w-full flex">
+                Education
+              </div>
+              {/* scholarly */}
+              <div className="w-full py-3 flex items-center justify-between">
+                <div>
+                  <p className="text-lg font-semibold">
+                    University Of Nigeria, Nsukka
+                  </p>
+                  <p className="text-slate-500">Collge</p>
+                </div>
+                {/* duration */}
+                <div className="border-2 px-4 py-1 rounded text-slate-500">
+                  2019-2023
+                </div>
+              </div>
+              {/* Abia Tech Hub */}
+              <div className="w-full py-3 flex items-center justify-between">
+                <div>
+                  <p className="text-lg font-semibold">GSS Pyakassa, Abuja</p>
+                  <p className="text-slate-500">High School</p>
+                </div>
+                {/* duration */}
+                <div className="border-2 px-4 py-1 rounded text-slate-500">
+                  2017-2019
+                </div>
+              </div>
+              {/* ECR Tech */}
+              <div className="w-full py-3 flex items-center justify-between">
+                <div>
+                  <p className="text-lg font-semibold">
+                    Caray College International
+                  </p>
+                  <p className="text-slate-500">Middle School</p>
+                </div>
+                {/* duration */}
+                <div className="border-2 px-4 py-1 rounded text-slate-500">
+                  2019-2021
+                </div>
+              </div>
             </div>
-            {/* scholarly */}
-            <div className="w-full py-3 flex items-center justify-between">
-              <div>
-                <p className="text-lg font-semibold">
-                  University Of Nigeria, Nsukka
-                </p>
-                <p className="text-slate-500">Collge</p>
-              </div>
-              {/* duration */}
-              <div className="border-2 px-4 py-1 rounded text-slate-500">
-                2019-2023
-              </div>
-            </div>
-            {/* Abia Tech Hub */}
-            <div className="w-full py-3 flex items-center justify-between">
-              <div>
-                <p className="text-lg font-semibold">GSS Pyakassa, Abuja</p>
-                <p className="text-slate-500">High School</p>
-              </div>
-              {/* duration */}
-              <div className="border-2 px-4 py-1 rounded text-slate-500">
-                2017-2019
-              </div>
-            </div>
-            {/* ECR Tech */}
-            <div className="w-full py-3 flex items-center justify-between">
-              <div>
-                <p className="text-lg font-semibold">
-                  Caray College International
-                </p>
-                <p className="text-slate-500">Middle School</p>
-              </div>
-              {/* duration */}
-              <div className="border-2 px-4 py-1 rounded text-slate-500">
-                2019-2021
-              </div>
-            </div>
-          </div>
+          </motion.div>
         </div>
 
         {/*  */}
--- components/works.tsx
+++ components/works.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import useOnScreen from "@/hooks/scroll";
+import { variantLeftToRight } from "@/hooks/variants";
+import { variantRightToLeft } from "@/hooks/variants";
+import { variantBottomToTop } from "@/hooks/variants";
 
 export default function Works() {
+  const [ref, isVisible] = useOnScreen({ threshold: 0.1 });
+  const [ref2, isVisible2] = useOnScreen({ theshold: 0.1 });
+  const [ref3, isVisible3] = useOnScreen({ threshold: 0.1 });
+
   return (
     <div className="pt-14 ">
       <div className="mx-8 md:mx-20">
@@ -17,59 +26,86 @@ export default function Works() {
       </div>
       <div className="grid md:grid-cols-3 gap-5 pt-5">
         {/* digital library */}
-        <Link
-          href="https://thuggers-library.vercel.app/"
-          className="relative h-[420px] md:h-[500px] bg-[#404258ba] cursor-pointer hover:scale-95 transition-all duration-500 flex flex-col justify-center"
+        <motion.div
+          // @ts-ignore
+          ref={ref}
+          initial="hidden"
+          animate={isVisible ? "visible" : "hidden"}
+          variants={variantLeftToRight}
+          transition={{ duration: 1 }}
         >
-          {/* Overlay */}
-          <div className="absolute inset-0 bg-black opacity-0 hover:opacity-80  transition-opacity duration-300 flex flex-col justify-center">
-            <p className="text-white text-3xl font-bold font-montserrat text-center z-50 relative">
-              Personalized Digital Library
-            </p>
-          </div>
+          <Link
+            href="https://thuggers-library.vercel.app/"
+            className="relative h-[420px] md:h-[500px] bg-[#404258ba] cursor-pointer hover:scale-95 transition-all duration-500 flex flex-col justify-center"
+          >
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black opacity-0 hover:opacity-80  transition-opacity duration-300 flex flex-col justify-center">
+              <p className="text-white text-3xl font-bold font-montserrat text-center z-50 relative">
+                Personalized Digital Library
+              </p>
+            </div>
 
-          {/* Image */}
-          <img
-            src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716878955/library-ps-compressed_lqmctm.jpg"
-            className="w-[90%] flex mx-auto "
-          />
-        </Link>
+            {/* Image */}
+            <img
+              src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716878955/library-ps-compressed_lqmctm.jpg"
+              className="w-[90%] flex mx-auto "
+            />
+          </Link>
+        </motion.div>
         {/* Blog */}
-        <Link
-          href="https://seven-lake.vercel.app/"
-          className="relative h-[420px] md:h-[500px] bg-[#404258ba] cursor-pointer hover:scale-95 transition-all duration-500 flex flex-col justify-center"
+        <motion.div
+          // @ts-ignore
+          ref={ref2}
+          initial="hidden"
+          animate={isVisible2 ? "visible" : "hidden"}
+          variants={variantBottomToTop}
+          transition={{ duration: 1 }}
         >
-          {/* Overlay */}
-          <div className="absolute inset-0 bg-black opacity-0 hover:opacity-80  transition-opacity duration-300 flex flex-col justify-center">
-            <p className="text-white text-3xl font-bold font-montserrat text-center z-50 relative">
-              Shopping Site
-            </p>
-          </div>
+          <Link
+            href="https://seven-lake.vercel.app/"
+            className="relative h-[420px] md:h-[500px] bg-[#404258ba] cursor-pointer hover:scale-95 transition-all duration-500 flex flex-col justify-center"
+          >
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black opacity-0 hover:opacity-80  transition-opacity duration-300 flex flex-col justify-center">
+              <p className="text-white text-3xl font-bold font-montserrat text-center z-50 relative">
+                Shopping Site
+              </p>
+            </div>
 
-          {/* Image */}
-          <img
-            src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716877932/newer/Screenshot-ps-compressed_ghkhic.jpg"
-            className="w-[90%] flex mx-auto "
-          />
-        </Link>
+            {/* Image */}
+            <img
+              src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716877932/newer/Screenshot-ps-compressed_ghkhic.jpg"
+              className="w-[90%] flex mx-auto "
+            />
+          </Link>
+        </motion.div>
         {/* crypto */}
-        <Link
-          href="https://swapp-nu.vercel.app/"
-          className="relative h-[420px] md:h-[500px] bg-[#404258ba] cursor-pointer hover:scale-95 transition-all duration-500 flex flex-col justify-center"
+        <motion.div
+          // @ts-ignore
+          ref={ref3}
+          initial="hidden"
+          animate={isVisible3 ? "visible" : "hidden"}
+          variants={variantRightToLeft}
+          transition={{ duration: 1 }}
         >
-          {/* Overlay */}
-          <div className="absolute inset-0 bg-black opacity-0 hover:opacity-80  transition-opacity duration-300 flex flex-col justify-center">
-            <p className="text-white text-3xl font-bold font-montserrat text-center z-50 relative">
-              Crypto Site
-            </p>
-          </div>
+          <Link
+            href="https://swapp-nu.vercel.app/"
+            className="relative h-[420px] md:h-[500px] bg-[#404258ba] cursor-pointer hover:scale-95 transition-all duration-500 flex flex-col justify-center"
+          >
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black opacity-0 hover:opacity-80  transition-opacity duration-300 flex flex-col justify-center">
+              <p className="text-white text-3xl font-bold font-montserrat text-center z-50 relative">
+                Crypto Site
+              </p>
+            </div>
 
-          {/* Image */}
-          <img
-            src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716621530/Screenshot_2024-05-24_101717_qots9g.png"
-            className="w-[90%] flex mx-auto "
-          />
-        </Link>
+            {/* Image */}
+            <img
+              src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716621530/Screenshot_2024-05-24_101717_qots9g.png"
+              className="w-[90%] flex mx-auto "
+            />
+          </Link>
+        </motion.div>
       </div>
     </div>
   );
