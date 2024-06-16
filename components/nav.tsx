6b4c0a7da9cbfe164ac1aa588f598e9553d2051f
Notjessand <jessennorom@gmail.com>
2024-06-16 09:03:27 -0500

    additional updates

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function nav() {
   return (
-    <div className="fixed w-full z-50 flex sm:px-20 py-4 sm:py-4 md:py-4 justify-between px-5 bg-white">
+    <div className="fixed w-full z-50 flex sm:px-20 py-3 sm:py-2 justify-between px-5 bg-white">
       <Link href={"/"} className="">
         <img
           src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716538239/Premium_Vector___Wolf_tech_modern_simple_logo_icon_ideas-removebg-preview_je6aex.png"
