db4659ed6b303494b226a5cdaab3de4b52758e38
Notjessand <jessennorom@gmail.com>
2024-09-11 06:33:05 -0500

    modification

--- app/data.js
+++ app/data.js
@@ -1,6 +1,6 @@
 export const Bio = `  Hello! I’m Jesse, and I find joy in crafting digital experiences
             that thrive on the web. My journey into web development began in
-            2017 when my fascination with gadgets ignited a passion for the
+            2019 when my fascination with gadgets ignited a passion for the
             underlying technologies. As a creative developer, I specialize in
             leveraging modern tools and frameworks like React.js and Next.js to
             bring ideas to life. With a solid foundation in Node.js and Express.js, I embrace the challenge of building scalable full-stack web applications.`;
