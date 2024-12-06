ec9d4947e085007129b0e124a9490f0f96ae232a
Notjessand <jessennorom@gmail.com>
2024-12-06 11:00:29 -0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     visible: { opacity: 1, x: 0 },
   };
 
-  const jaja = "gcdscjfvger";
+  const jaja = "gcdscjddddfvger";
 
   return (
     <main className="bg-slate-50 overflow-hidden">
