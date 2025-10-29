c015046a2c4bf23484d34554465e74539d7dac07
Notjessand <jessennorom@gmail.com>
2025-10-29 17:33:59 +0100

    x

--- components/support.tsx
+++ components/support.tsx
@@ -4,17 +4,21 @@ import Stack from "./ui/stack";
 
 export default function Support() {
   return (
-    <div className=" mt-10 sm:h-96  h-[430px]  bg-[url('/bg.jpg')]  bg-cover relative flex flex-col justify-center bg-fixed">
+    <div className=" mt-10 py-24  bg-[url('/bg.jpg')]  bg-cover relative flex flex-col justify-center bg-fixed">
       {/* overlay */}
       <div className="absolute bg-black h-full w-full opacity-70"></div>
-      <div className="z-50 pl-6 md:pl-16 pr-2">
+      <div className="z-50 pl-6 md:pl-16 pr-2 mb-20">
         <p className="text-white text-4xl font-montserrat font-semibold">
-          Need help with professional <br className="hidden md:block"/> support? Let&apos;s work together!
+          Need help with professional <br className="hidden md:block" />{" "}
+          support? Let&apos;s work together!
         </p>
-        <Link href="mailto:jessennorom@gmail.com" className="text-white mt-7 flex">
+        <Link
+          href="mailto:jessennorom@gmail.com"
+          className="text-white mt-7 flex"
+        >
           jessennorom@gmail.com
         </Link>
-        <Stack/>
+        <Stack />
       </div>
     </div>
   );
