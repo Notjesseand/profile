1a35ee2b577e8dda2771bce860b3d40141285d3d
Notjessand <jessennorom@gmail.com>
2025-10-29 18:46:45 +0100

    x

--- components/support.tsx
+++ components/support.tsx
@@ -4,7 +4,7 @@ import Stack from "./ui/stack";
 
 export default function Support() {
   return (
-    <div className=" mt-10 py-24 bg-top  bg-[url('/bg.jpg')]  bg-cover relative flex flex-col justify-center bg-fixed">
+    <div className=" mt-10 py-24 bg-top  bg-[url(https://res.cloudinary.com/dv62ty87r/image/upload/v1761759936/bg_c84ss2.jpg)]  bg-cover relative flex flex-col justify-center bg-fixed">
       {/* overlay */}
       <div className="absolute bg-black h-full w-full opacity-70"></div>
       <div className="z-50 pl-6 md:pl-16 pr-2 mb-20">
